4413f88ace92894a06999e57013b812ebf9a5e5e
Hendi Lie <lie.hendi@gmail.com>
2017-11-23 11:21:15 +1000

    updates

--- dm_tools.py
+++ dm_tools.py
@@ -1,8 +1,5 @@
 import numpy as np
 import pandas as pd
-import pydot
-from io import StringIO
-from sklearn.tree import export_graphviz
 
 def data_prep():
     # read the pva97nk dataset
@@ -36,21 +33,22 @@ def data_prep():
     return df
 
 def analyse_feature_importance(dm_model, feature_names, n_to_display=20):
-	# grab feature importances from the model
-	importances = dm_model.feature_importances_
-	
-	# sort them out in descending order
-	indices = np.argsort(importances)
-	indices = np.flip(indices, axis=0)
+    # grab feature importances from the model
+    importances = dm_model.feature_importances_
 
-	# limit to 20 features, you can leave this out to print out everything
-	indices = indices[:n_to_display]
+    # sort them out in descending order
+    indices = np.argsort(importances)
+    indices = np.flip(indices, axis=0)
 
-	for i in indices:
-	    print(feature_names[i], ':', importances[i])
+    # limit to 20 features, you can leave this out to print out everything
+    indices = indices[:n_to_display]
+
+    for i in indices:
+       print(feature_names[i], ':', importances[i])
 
 def visualize_decision_tree(dm_model, feature_names, save_name):
-	dotfile = StringIO()
-	export_graphviz(dm_model, out_file=dotfile, feature_names=feature_names)
-	graph = pydot.graph_from_dot_data(dotfile.getvalue())
-	graph[0].write_png(save_name) # saved in the following file
+    import pydot
+    dotfile = StringIO()
+    export_graphviz(dm_model, out_file=dotfile, feature_names=feature_names)
+    graph = pydot.graph_from_dot_data(dotfile.getvalue())
+    graph[0].write_png(save_name) # saved in the following file
